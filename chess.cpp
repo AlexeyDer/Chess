65ddb0562b26039aa4f32b824613d2539a8d5901
AlexeyDer <hottelmich@gmail.com>
2019-03-09 21:27:13 +0700

    Building a sipmle board

--- chess.cpp
+++ chess.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
+#include <cstdlib>
+using namespace std;
 
-#define H 9
-#define W 9
+#define SIZE 8
+#define WHITE_SQUARE 0xDB
+#define BLACK_SQUARE 0xFF
 
 int main()
 {
+    cout << "Wecome you in my Chess GAME!\n\n";
+    cout << "The DECK:\n\n ";
+
     while(1)
     {
-        cout << "Wecome you in my Chess GAME!" << endl << endl;
-        cout << "The DECK: " << endl;
+        cout << "   A     B     C     D     E     F     G     H\n\n";
         
-        for(int i = 0; i < H; i++)
-        {
-            
-        }
+        for(int i = 1; i <= SIZE; i++)
+         {  
+            cout << "   ";
+            for(int j = 1; j <= SIZE;j++)
+            {
+                if((i+j) % 2)
+                {
+                    cout << "-";
+                } else {
+                    cout << "x";
+                }
+                cout << "     ";
+            }
+            cout << " " << i << endl;
+         }       
+
+         char c = cin.get();
+         if(c == 27)
+            exit(0);
+         
     }
     return 0;
 }

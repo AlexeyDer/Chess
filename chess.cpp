57cbb9b466c348f566f91bec85aebabc85d71935
AlexeyDer <hottelmich@gmail.com>
2019-03-09 23:32:10 +0700

    Peredelal simple board pod obichniy file with input in file

--- chess.cpp
+++ chess.cpp
@@ -6,47 +6,42 @@
 using namespace std;
 
 #define SIZE 8
-#define WHITE_SQUARE 0xDB
-#define BLACK_SQUARE 0xFF
+
 
 int main()
 {
-    cout << "Wecome you in my Chess GAME!\n\n";
-    cout << "X - White\n- is black\n";
-
     while(1)
     {
-        cout << "   A     B     C     D     E     F     G     H\n\n";
+        ofstream myfile("/home/alexeyder/Documents/progs/Chess/hello.txt");
+        myfile << "Wecome you in my Chess GAME!\n\n";
+        myfile << "X - White\n- is black\n";
+        myfile << "   A     B     C     D     E     F     G     H\n\n";
         
         for(int i = 1; i <= SIZE; i++)
          {  
-            cout << "   ";
+            myfile << "   ";
             for(int j = 1; j <= SIZE;j++)
             {
                 if((i+j) % 2)
                 {
-                    cout << "-";
+                    myfile << "-";
                 } else {
-                    cout << "X";
+                    myfile << "X";
                 }
-                cout << "     ";
+                myfile << "     ";
             }
-            cout << " " << i << endl;
+            myfile << " " << i << endl;
          }       
-
+         
+         
          char c = cin.get();
          if(c == 27)
-            break;       
+         {
+            myfile.close();
+            break;  
+         }     
+
     }
-
-    ofstream myfile("/home/alexeyder/Documents/progs/Chess/Website.html");
-
-    myfile.open ("/home/alexeyder/Documents/progs/Chess/Website.html");
-    myfile << "<!DOCTYPE html><html><head></head><body>" << endl;
-    myfile << "<h1>Wecome you in my Chess GAME!\n\n</h1>";
-
-    myfile << "</body></html>";
-    myfile.close();
 
     return 0;
 }

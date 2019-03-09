b53520af8f71b2488afb5a4af5cbc5883ce9a4ea
AlexeyDer <hottelmich@gmail.com>
2019-03-09 20:37:24 +0700

    Starting write Chess Game!

---
+++ chess.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#define H 9
+#define W 9
+
+int main()
+{
+    while(1)
+    {
+        cout << "Wecome you in my Chess GAME!" << endl << endl;
+        cout << "The DECK: " << endl;
+        
+        for(int i = 0; i < H; i++)
+        {
+            
+        }
+    }
+    return 0;
+}

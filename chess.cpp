40424a142ee61c1f596cccfa4bdb97b83eac3809
AlexeyDer <hottelmich@gmail.com>
2019-03-11 09:23:33 +0700

    Create normal program with figures

--- chess.cpp
+++ chess.cpp
@@ -13,24 +13,31 @@ int main()
     {
         ofstream myfile("/home/alexeyder/Documents/progs/Chess/hello.txt");
         myfile << "Wecome you in my Chess GAME!\n\n";
-        myfile << "X - White\n- is black\n";
+      //  myfile << "X - White\n- is black\n";
         myfile << "   A     B     C     D     E     F     G     H\n\n";
         
-        for(int i = 1; i <= SIZE; i++)
-         {  
-            myfile << "   ";
+        // for(int i = 1; i <= SIZE; i++)
+        //  {  
+        //     myfile << "   ";
             for(int j = 1; j <= SIZE;j++)
             {
-                if((i+j) % 2)
-                {
-                    myfile << "-";
-                } else {
-                    myfile << "X";
-                }
-                myfile << "     ";
+                if(j == 1)
+                myfile << "   r     n     b     q     k     b     n     r   " << j << endl;
+
+                if(j > 2 && j < 7)
+                  myfile << "                                                 " << j << endl;
+                if(j == 2)
+                myfile << "   p     p     p     p     p     p     p     p   " << j << endl;
+
+                if(j == 7)
+                myfile << "   P     P     P     P     P     P     P     P   " << j << endl;
+
+                if(j == 8)
+                myfile << "   R     N     B     Q     K     B     N     R   " << j << endl;
+                 
             }
-            myfile << " " << i << endl;
-         }       
+       
+      //   }       
          
          
          char c = cin.get();

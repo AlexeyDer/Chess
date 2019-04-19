342771a9c1d297ce18428d2ec06724bf93ae8f26
AlexeyDer <hottelmich@gmail.com>
2019-04-19 21:19:57 +0700

    Added func move figurs and func check input data

--- src/board.cpp
+++ src/board.cpp
@@ -1,35 +1,42 @@
 #include "board.h"
 
-int check_symbol(char a[], int n)
+void Move(char deck[8][8], int l)
 {
-    switch (a[n]) {
-    case 'a':
-        return 0;
-    case 'b':
-        return 1;
-    case 'c':
-        return 2;
-    case 'd':
-        return 3;
-    case 'e':
-        return 4;
-    case 'f':
-        return 5;
-    case 'g':
-        return 6;
-    case 'h':
-        return 7;
-    }
-    return -1;
+    char fir[6], sec[6];
+    int x1, x2, y1, y2;
+    cout << ++l << ".";
+    cin >> fir >> sec;
+
+    CheckInput(fir, x1, x2, y1, y2);
+    deck[x2][y2] = deck[x1][y1];
+    deck[x1][y1] = 32;
+
+    cout << ++l << ".";
+    cin >> fir;
+    cout << endl;
+    PrintBoard(deck);
+    readTxt();
+
+    CheckInput(sec, x1, x2, y1, y2);
+    deck[x2][y2] = deck[x1][y1];
+    deck[x1][y1] = 32;
+
+    cin >> fir;
+    cout << endl;
+    PrintBoard(deck);
+    readTxt();
 }
 
-int check_reg(char one, char two)
+void CheckInput(char a[], int& x1, int& x2, int& y1, int& y2)
 {
-    if ((one <= 'Z' && one >= 'A') && (two <= 'Z' && two >= 'A')) {
-        return 0;
+    x1 = (int)a[1] - 49;
+    x2 = (int)a[4] - 49;
+    y1 = (int)a[0] - 97;
+    y2 = (int)a[3] - 97;
+
+    if ((x1 < 0) || (x1 > 7) || (x2 < 0) || (x2 > 7) || (y1 < 0) || (y1 > 7)
+        || (y2 < 0) || (y2 > 7)) {
+        cout << "Ups, you are around the board!\n";
+        exit(-1);
     }
-    if ((one <= 'z' && one >= 'a') && (two <= 'z' && two >= 'a')) {
-        return 0;
-    }
-    return 1;
 }

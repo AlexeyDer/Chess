4166132e0286dae04784cf983eeeb55012020cf6
AlexeyDer <hottelmich@gmail.com>
2019-04-19 20:50:47 +0700

    added symbols to figures

--- src/board_print_plain.cpp
+++ src/board_print_plain.cpp
@@ -1,13 +1,16 @@
 #include "board_print_plain.h"
 
-void PrintBoard(char deck[size][size])
+void PrintBoard(char deck[S][S])
 {
-    ofstream myfile("/home/alexeyder/Documents/progs/Chess/hey.txt");
-    myfile << "A B C D E F G H\n";
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++)
+    ofstream myfile("/home/alexeyder/Documents/progs/Chess/output.txt");
+    myfile << "BIG is white, SMALL is black!\n";
+    myfile << "  A B C D E F G H\n";
+    for (int i = 0; i < S; i++) {
+        myfile << i + 1 << " ";
+        for (int j = 0; j < S; j++)
             myfile << deck[i][j] << " ";
         myfile << i + 1 << endl;
     }
+    myfile << "  A B C D E F G H\n";
     myfile.close();
 }

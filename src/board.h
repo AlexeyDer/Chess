ec4ff4e0781e8c32b12c734196111a97c474d450
AlexeyDer <hottelmich@gmail.com>
2019-04-19 21:22:25 +0700

    Create header which contains basic libs for all programm and added here protocols of a functions

---
+++ src/board.h
@@ -0,0 +1,19 @@
+#ifndef _BOARD_H
+#define _BOARD_H
+using namespace std;
+#define S 8
+
+#include "board_print_plain.h"
+#include "board_read.h"
+
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+void readTxt();
+void PrintBoard(char deck[S][S]);
+void Move(char deck[8][8], int l);
+void CheckInput(char a[], int& x1, int& x2, int& y1, int& y2);
+
+#endif

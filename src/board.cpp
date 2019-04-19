16d41fb478d27eb499a04bafc2d4f3c9122edeb3
AlexeyDer <hottelmich@gmail.com>
2019-04-19 18:36:23 +0700

    Add func for checking symbols

---
+++ src/board.cpp
@@ -0,0 +1,24 @@
+#include "board.h"
+
+int check_symbol(char a[], int n)
+{
+    switch (a[n]) {
+    case 'a':
+        return 0;
+    case 'b':
+        return 1;
+    case 'c':
+        return 2;
+    case 'd':
+        return 3;
+    case 'e':
+        return 4;
+    case 'f':
+        return 5;
+    case 'g':
+        return 6;
+    case 'h':
+        return 7;
+    }
+    return -1;
+}

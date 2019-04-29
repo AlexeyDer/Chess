ff13d04870075624ed530a07b80c0078c2b7d4a0
AlexeyDer <hottelmich@gmail.com>
2019-04-29 15:32:12 +0700

    Rebuild on others functions

---
+++ src/board.c
@@ -0,0 +1,46 @@
+#include "board.h"
+
+void Fill(char move[5], int moveint[4])
+{
+    moveint[0] = move[0] - 97;
+    moveint[1] = move[1] - 49;
+    moveint[2] = move[3] - 97;
+    moveint[3] = move[4] - 49;
+}
+
+void Move(char deck[8][8], int l, int step)
+{
+    char move[5];
+    int moveInt[4];
+
+    if (!step) {
+        printf("%d.", ++l);
+        scanf("%s", move);
+        Fill(move, moveInt);
+    }
+
+    for (int i = 0; i < 4; i++)
+        printf("%d ", moveInt[i]);
+    printf("\n");
+    if (!CheckInput(moveInt))
+        exit(3);
+
+    makeMove(deck, moveInt);
+    PrintBoard(deck);
+}
+
+void makeMove(char deck[S][S], int move[4])
+{
+    deck[move[3]][move[2]] = deck[move[1]][move[0]];
+    deck[move[1]][move[0]] = ' ';
+}
+
+int CheckInput(int move[])
+{
+    if ((move[0] < 0) || (move[0] > 7) || (move[1] < 0) || (move[1] > 7)
+        || (move[2] < 0) || (move[2] > 7) || (move[3] < 0) || (move[3] > 7)) {
+        return 0;
+    } else {
+        return 1;
+    }
+}

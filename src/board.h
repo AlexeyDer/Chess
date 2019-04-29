f84e33329f0a846fbe25ed7b344dcb4b56dc7e2e
AlexeyDer <hottelmich@gmail.com>
2019-04-29 15:31:17 +0700

    Delete C++ lib and add C

--- src/board.h
+++ src/board.h
@@ -1,19 +1,17 @@
 #ifndef _BOARD_H
 #define _BOARD_H
-using namespace std;
 #define S 8
 
-#include "board_print_plain.h"
-#include "board_read.h"
-
-#include <cstring>
-#include <fstream>
-#include <iostream>
+#include "PrintBoard.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <vector>
+#include <string.h>
 
-void readTxt();
 void PrintBoard(char deck[S][S]);
-void Move(char deck[8][8], int l);
-void CheckInput(char a[], int& x1, int& x2, int& y1, int& y2);
+void Move(char deck[8][8], int l, int step);
+
+int CheckInput(int move[]);
+void Fill(char move[5], int moveint[4]);
+void makeMove(char deck[S][S], int move[]);
+
 #endif

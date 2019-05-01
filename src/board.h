633ad12c095ebd5f1c43b4928c84fc14172f8449
AlexeyDer <hottelmich@gmail.com>
2019-05-01 16:39:49 +0700

    Add protocol func step pawn and rook, delete cpp lib

--- src/board.h
+++ src/board.h
@@ -3,15 +3,22 @@
 #define S 8
 
 #include "PrintBoard.h"
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void PrintBoard(char deck[S][S]);
-void Move(char deck[8][8], int l, int step);
+int Move(char deck[8][8], int l);
 
 int CheckInput(int move[]);
 void Fill(char move[5], int moveint[4]);
 void makeMove(char deck[S][S], int move[]);
+
+int wPawn(char deck[8][8], int move[4]);
+int bPawn(char deck[8][8], int move[4]);
+int Rook(char deck[8][8], int moveInt[4]);
+
+int check_figure(char deck[8][8], int move[4]);
 
 #endif
